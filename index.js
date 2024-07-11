9a1fb58b028c9fe0b25d613e6fc8b83afe0b18f1
JerardSoh <jerard.soh@gmail.com>
2024-07-11 17:01:11 +0800

    assignmennt3

--- index.js
+++ index.js
@@ -252,8 +252,169 @@ app.post("/GetTaskbyState", async (req, res) => {
 
         await connection.commit();
 
+        res.status(200).json(tasksArray);
+    } catch (error) {
+        await connection.rollback();
+        console.error("Error:", error.message);
+        res.status(500).json({
+            message: "Internal server error",
+        });
+    } finally {
+        connection.release();
+    }
+});
+
+// PromoteTask2Done route
+app.patch("/PromoteTask2Done", async (req, res) => {
+    const { username, password, Task_id, Task_app_Acronym } = req.body;
+
+    // Check if mandatory fields are provided
+    if (!username || !password || !Task_id || !Task_app_Acronym) {
+        return res
+            .status(400)
+            .json({ message: "Invalid or missing mandatory fields" });
+    }
+
+    // Validate that all required fields are strings
+    if (
+        typeof username !== "string" ||
+        typeof password !== "string" ||
+        typeof Task_id !== "string" ||
+        typeof Task_app_Acronym !== "string"
+    ) {
+        return res.status(400).json({
+            message: "Invalid input. All required fields must be strings.",
+        });
+    }
+
+    const connection = await db.getConnection();
+
+    try {
+        await connection.beginTransaction();
+
+        // Check if app exists
+        const [app] = await connection.query(
+            "SELECT * FROM App WHERE App_Acronym = ?",
+            [Task_app_Acronym]
+        );
+        if (app.length === 0) {
+            return res.status(404).json({ message: "App not found" });
+        }
+
+        // Check if task exists
+        const [tasks] = await connection.query(
+            "SELECT * FROM Task WHERE Task_id = ?",
+            [Task_id]
+        );
+        if (tasks.length === 0) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        const task = tasks[0];
+
+        // Check if task is in 'doing' state
+        if (task.Task_state !== "doing") {
+            return res.status(400).json({
+                message: "Task is not in 'doing' state",
+            });
+        }
+
+        // Check username exists
+        const [users] = await connection.query(
+            "SELECT * FROM users WHERE username = ?",
+            [username]
+        );
+        const user = users[0];
+        if (!user) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        // Check password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        // Check if user can move task from doing to done
+        const [appPermitDoing] = await db.query(
+            "SELECT App_permit_Doing FROM App WHERE App_Acronym = ? ",
+            [Task_app_Acronym]
+        );
+        const appPermitDoingGroup = appPermitDoing[0].App_permit_Doing;
+        const isInGroup = await checkGroup(user.username, appPermitDoingGroup);
+        if (!isInGroup) {
+            return res.status(403).json({
+                message: "User does not have permission to move task to done",
+            });
+        }
+
+        // Update the task state to 'done'
+        await connection.execute(
+            "UPDATE Task SET Task_state = 'done' WHERE Task_id = ?",
+            [Task_id]
+        );
+
+        // Create notes with format of [Task_createDate, Task_state] Task_notes
+        const unformattedTask_createDate = new Date();
+        const addTask_notes = `[${unformattedTask_createDate}, '${task.Task_state}'] ${user.username} has completed the Task.\n ##########################################################\n`;
+
+        // Update Task_notes
+        await connection.execute(
+            "UPDATE Task SET Task_notes = CONCAT(?, Task_notes) WHERE Task_id = ? ",
+            [addTask_notes, Task_id]
+        );
+
+        // Update Task_owner
+        await connection.execute(
+            "UPDATE Task SET Task_owner = ? WHERE Task_id = ?",
+            [user.username, Task_id]
+        );
+
+        // Start of sending email section
+        const [appPermitDone] = await db.query(
+            "SELECT App_permit_Done FROM App WHERE App_Acronym = ? ",
+            [Task_app_Acronym]
+        );
+
+        const appPermitDoneGroup = appPermitDone[0].App_permit_Done;
+        // check if appPermitDoneGroup is not empty
+        if (appPermitDoneGroup) {
+            // get all users from the done group
+            const [users] = await db.query(
+                "SELECT username FROM usergroup WHERE groupname = ?",
+                [appPermitDoneGroup]
+            );
+            const usernames = users.map((user) => user.username);
+            // Get all user emails
+            const [emails] = await db.query(
+                "SELECT email FROM users WHERE username IN (?) AND status = true",
+                [usernames]
+            );
+
+            // Send email to all users in the group, if there is any
+            if (emails.length > 0) {
+                const transporter = nodemailer.createTransport({
+                    service: "outlook",
+                    auth: {
+                        user: process.env.EMAIL,
+                        pass: process.env.EMAIL_PASSWORD,
+                    },
+                });
+
+                const mailOptions = {
+                    from: process.env.EMAIL,
+                    to: emails.map((email) => email.email).join(","),
+                    subject: `${task.Task_id} has been completed!`,
+                    text: `Task ${task.Task_Name} is completed by ${Task_owner}`,
+                };
+
+                transporter.sendMail(mailOptions);
+            }
+        }
+
+        await connection.commit();
+
         res.status(200).json({
-            tasksArray,
+            Task_id,
         });
     } catch (error) {
         await connection.rollback();
