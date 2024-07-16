ba5ea6d99ae28c08c140a2889d16cc94230642c8
JerardSoh <jerard.soh@gmail.com>
2024-07-16 09:06:28 +0800

    404

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const routes = require("./routes");
 app.use(express.json());
 app.use("/", routes);
 
+app.all("*", (req, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
